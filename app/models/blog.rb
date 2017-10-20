c8f81ab3b42bae80194c58d2207553669f0ea3fd
yatchan <jazzguitar.tokyo@gmail.com>
2017-10-20 11:57:37 +0000

    first commit

---
+++ app/models/blog.rb
@@ -0,0 +1,6 @@
+class Blog < ActiveRecord::Base
+    validates :title, presence: true
+    belongs_to :user
+    has_many :comments, dependent: :destroy
+    mount_uploader :image, ImageUploader
+end
---
+++ config/routes.rb
@@ -0,0 +1,39 @@
+Rails.application.routes.draw do
+
+  get 'notifications/index'
+
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+
+  resources :blogs do
+    resources :comments
+    post :confirm, on: :collection
+  end
+
+  resources :contacts, only: [:new, :create] do
+    collection do
+      post :confirm
+    end
+  end
+
+  root 'top#index'
+
+  resources :users, only: [:index, :show]
+  resources :relationships, only: [:create, :destroy]
+
+  resources :conversations do
+    resources :messages
+  end
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+end
