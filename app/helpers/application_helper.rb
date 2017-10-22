4c7571fed87d7a11e7a4f29ed960f2599f5d7737
yatchan <jazzguitar.tokyo@gmail.com>
2017-10-22 07:29:56 +0000

    プロフィールの画像サイズ変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name , :size=>"50x50") if user.avatar?
+    return image_tag(user.avatar, alt: user.name, :size=>"50x50") if user.avatar?
+
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name, :size=>"50x50"
+    image_tag(img_url, alt: user.name, :size=>"50x50")
   end
 end
